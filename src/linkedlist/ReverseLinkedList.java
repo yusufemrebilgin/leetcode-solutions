9c32e22449786abe48b9c426ef34542403031332
Yusuf Emre Bilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-11-30 14:58:31 +0300

    add ReverseLinkedList.java

---
+++ src/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,35 @@
+package linkedlist;
+/*
+ * Given the `head` of a singly linked list, reverse the list, and return the reversed list.
+ * Example 1:
+ *  - Input:    head = [1,2,3,4,5]
+ *  - Output:          [5,4,3,2,1]
+ * Example 2:
+ *  - Input:    head = [1,2]
+ *  - Output:          [2,1]
+ * Example 3:
+ *  - Input:    head = []
+ *  - Output:          []
+ */
+class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode iterator = head;
+        head = null;
+        while (iterator != null) {
+            ListNode temp = iterator;
+            iterator = iterator.next;
+            temp.next = head;
+            head = temp;
+        }
+        return head;
+    }
+}
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
